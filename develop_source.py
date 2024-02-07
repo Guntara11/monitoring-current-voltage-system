6c337efc7cd61c06835e9cb01017e201acf1d995
ega guntara <ega.guntara394@gmail.com>
2024-02-07 22:56:37 +0700

    finishing counter params

--- develop_source.py
+++ develop_source.py
@@ -3,17 +3,20 @@ import time
 from utils import LineCalculation
 
 
-# Function to get real data
-def get_real_data(parsed_data):
-    return parsed_data[:6]
+# # Function to get real data
+# def get_real_data(parsed_data):
+#     return parsed_data[:6]
 
-# Function to get imaginary data
-def get_imag_data(parsed_data):
-    return parsed_data[6:12]
+# # Function to get imaginary data
+# def get_imag_data(parsed_data):
+#     return parsed_data[6:12]
 
-# Function to get complex data
-def get_complex_data(parsed_data):
-    return parsed_data[12:]
+# # Function to get complex data
+# def get_complex_data(parsed_data):
+#     return parsed_data[12:]
+
+# def get_line1_IN_data(parsed_data):
+#     return parsed_data[:2]
 
 while True:
     # Generate random values
@@ -53,23 +56,17 @@ while True:
     # Create LineCalculation object and pass random values
     line_calc = LineCalculation()
     line_calc.calculate_values(LINE1_U1, LINE1_U2, LINE1_U3, LINE1_Ang_U1, LINE1_Ang_U2, LINE1_Ang_U3,
-                                LINE1_IL1, LINE1_U2, LINE1_IL1, LINE1_Ang_I1, LINE1_Ang_I2, LINE1_Ang_I2,
+                                LINE1_IL1, LINE1_IL2, LINE1_IL3, LINE1_Ang_I1, LINE1_Ang_I2, LINE1_Ang_I3,
                                 LINE1_z0z1_mag, LINE1_z0z1_ang)
 
-    # Get parsed data
+    # Get real and imag  data
     real_data = line_calc.get_real_data()
     imag_data = line_calc.get_imag_data()
-    complex_data = line_calc.get_complex_data()
-
 
     # Unpack real data
     IL1_Real, IL2_Real, IL3_Real, V1_Real, V2_Real, V3_Real = real_data
-
     # Unpack imaginary data
     IL1_Imag, IL2_Imag, IL3_Imag, V1_Imag, V2_Imag, V3_Imag = imag_data
-
-    # Unpack complex data
-    IL1_Complex, IL2_Complex, IL3_Complex, V1_Complex, V2_Complex, V3_Complex, LINE1_IN_Complex = complex_data
 
     # Printing data
     print("Real Data:", real_data)
@@ -80,9 +77,73 @@ while True:
     print("IL1_Imag = {0}\nIL2_Imag = {1}\nIL3_Imag = {2}".format(IL1_Imag, IL2_Imag, IL3_Imag))
     print("V1_Imag = {0}\nV2_Imag = {1}\nV3_Imag = {2}".format(V1_Imag, V2_Imag, V3_Imag))
 
+
+    complex_data = line_calc.get_complex_data()
+
+    # Unpack complex data
+    IL1_Complex, IL2_Complex, IL3_Complex, V1_Complex, V2_Complex, V3_Complex = complex_data
+
     print("Complex Data:", complex_data)
     print("IL1_Complex = {0}\nIL2_Complex = {1}\nIL3_Complex = {2}".format(IL1_Complex, IL2_Complex, IL3_Complex))
     print("V1_Complex = {0}\nV2_Complex = {1}\nV3_Complex = {2}".format(V1_Complex, V2_Complex, V3_Complex))
-    print("LINE1_IN_Complex =", LINE1_IN_Complex)
 
-    time.sleep(2)
+
+    line1_IN_data = line_calc.get_line1_IN_data()
+
+    #Unpack LINE1_IN_data 
+    LINE1_IN_Real, LINE1_IN_Imag, LINE1_IN_Complex, LINE1_IN_Mag, LINE1_IN_Ang = line1_IN_data
+    print("LINE_IN data :", line1_IN_data)
+    print("LINE1_IN_Complex = {0}\nLINE1_IN_Imag = {1}\nLiNE1_IN_Real = {2}\nLINE1_IN_Mag{3}\nLiNE1_IN_Ang".format(LINE1_IN_Complex, LINE1_IN_Imag, LINE1_IN_Real, LINE1_IN_Mag, LINE1_IN_Ang))
+
+    k0 = line_calc.get_LINE1_k0()
+    n0 = line_calc.get_LINE1_n0()
+    product_result = line_calc.get_LINE1_PR()
+    print("k0 = {0}\nn0 = {1}\nproduct result = {2}".format(k0, n0, product_result))
+
+    
+    za_data = line_calc.get_ZA_data()
+    #unpack data
+    LINE1_ZA_Real, LINE1_ZA_Imag, LINE1_ZA_Mag, LINE1_ZA_Ang, LINE1_ZA_R, LINE1_ZA_X = za_data
+    #print data 
+    print("LINE1_ZA_data", za_data)
+    print("LINE1_ZA_Real = {0}\nLINE1_ZA_Imag = {1}\nLINE1_ZA_Mag = {2}\nLINE1_ZA_Ang = {3}\nLINE1_ZA_R = {4}\nLINE1_ZA_X = {5}"
+          .format(LINE1_ZA_Real, LINE1_ZA_Imag, LINE1_ZA_Mag, LINE1_ZA_Ang, LINE1_ZA_R, LINE1_ZA_X))
+    
+    zb_data = line_calc.get_ZB_data()
+    #unpack data
+    LINE1_ZB_Real, LINE1_ZB_Imag, LINE1_ZB_Mag, LINE1_ZB_Ang, LINE1_ZB_R, LINE1_ZB_X = zb_data
+    print("LINE1_ZB_data", zb_data)
+    print("LINE1_ZB_Real = {0}\nLINE1_ZB_Imag = {1}\nLINE1_ZB_Mag = {2}\nLINE1_ZB_Ang = {3}\nLINE1_ZB_R = {4}\nLINE1_ZB_X = {5}"
+          .format(LINE1_ZB_Real, LINE1_ZB_Imag, LINE1_ZB_Mag, LINE1_ZB_Ang, LINE1_ZB_R, LINE1_ZB_X))
+    
+    zc_data = line_calc.get_ZC_data()
+    #unpack data
+    LINE1_ZC_Real, LINE1_ZC_Imag, LINE1_ZC_Mag, LINE1_ZC_Ang, LINE1_ZC_R, LINE1_ZC_X = zc_data
+    print("LINE1_ZC_data", zc_data)
+    print("LINE1_ZC_Real = {0}\nLINE1_ZC_Imag = {1}\nLINE1_ZC_Mag = {2}\nLINE1_ZC_Ang = {3}\nLINE1_ZC_R = {4}\nLINE1_ZC_X = {5}"
+        .format(LINE1_ZC_Real, LINE1_ZC_Imag, LINE1_ZC_Mag, LINE1_ZC_Ang, LINE1_ZC_R, LINE1_ZC_X))
+    
+    zab_data = line_calc.get_ZAB_data()
+    #unpack data
+    LINE1_ZAB_Mag, LINE1_ZAB_Ang, LINE1_ZAB_R, LINE1_ZAB_X = zab_data
+    #print data 
+    print("LINE1_ZAB_data", zab_data)
+    print("LINE1_ZAB_Mag = {0}\nLINE1_ZAB_Ang = {1}\nLINE1_ZAB_R = {2}\nLINE1_ZAB_X = {3}".format(LINE1_ZAB_Mag, LINE1_ZAB_Ang, LINE1_ZAB_R, LINE1_ZAB_X))
+
+    zbc_data = line_calc.get_ZBC_data()
+    #unpack data
+    LINE1_ZBC_Mag, LINE1_ZBC_Ang, LINE1_ZBC_R, LINE1_ZBC_X = zbc_data
+    #print data 
+    print("LINE1_ZBC_data", zbc_data)
+    print("LINE1_ZBC_Mag = {0}\nLINE1_ZBC_Ang = {1}\nLINE1_ZBC_R = {2}\nLINE1_ZBC_X = {3}".format(LINE1_ZBC_Mag, LINE1_ZBC_Ang, LINE1_ZBC_R, LINE1_ZBC_X))
+
+    zca_data = line_calc.get_ZCA_data()
+    #unpack data
+    LINE1_ZCA_Mag, LINE1_ZCA_Ang, LINE1_ZCA_R, LINE1_ZCA_X = zca_data
+    #print data 
+    print("LINE1_ZCA_data", zca_data)
+    print("LINE1_ZCA_Mag = {0}\nLINE1_ZCA_Ang = {1}\nLINE1_ZCA_R = {2}\nLINE1_ZCA_X = {3}".format(LINE1_ZCA_Mag, LINE1_ZCA_Ang, LINE1_ZCA_R, LINE1_ZCA_X))
+    
+
+    
+    time.sleep(10)
--- utils.py
+++ utils.py
@@ -1,57 +1,154 @@
 import math
-
+import cmath
 class LineCalculation:
     def __init__(self):
         self.real_data = []
         self.imag_data = []
         self.complex_data = []
+        self.line1_IN_data = []
+        self.line1_k0 = None
+        self.line1_n0 = None
+        self.data_ZA = []
+        self.data_ZB = []
+        self.data_ZC = []
+        self.data_ZAB = []
+        self.data_ZBC = []
+        self.data_ZCA = []
 
     def calculate_values(self, LINE1_U1, LINE1_U2, LINE1_U3, LINE1_Ang_U1, LINE1_Ang_U2, LINE1_Ang_U3,
                          LINE1_IL1, LINE1_IL2, LINE1_IL3, LINE1_Ang_I1, LINE1_Ang_I2, LINE1_Ang_I3,
                          LINE1_z0z1_mag, LINE1_z0z1_ang):
-        self.calculate_IL_real_imag(LINE1_IL1, LINE1_Ang_I1, LINE1_IL2, LINE1_Ang_I2, LINE1_IL3, LINE1_Ang_I3)
-        self.calculate_V_real_imag(LINE1_U1, LINE1_Ang_U1, LINE1_U2, LINE1_Ang_U2, LINE1_U3, LINE1_Ang_U3)
-        self.create_complex_data()
+        self._calculate_IL_real_imag(LINE1_IL1, LINE1_Ang_I1, LINE1_IL2, LINE1_Ang_I2, LINE1_IL3, LINE1_Ang_I3)
+        self._calculate_V_real_imag(LINE1_U1, LINE1_Ang_U1, LINE1_U2, LINE1_Ang_U2, LINE1_U3, LINE1_Ang_U3)
+        self._create_complex_data()
+        self.calculate_LINE1_IN()
+        self.calculate_k0_n0(LINE1_z0z1_mag, LINE1_z0z1_ang)
+        self.calculate_mag_ang_R_X('A')
+        self.calculate_mag_ang_R_X('B')
+        self.calculate_mag_ang_R_X('C')
+        self.combine_calculate_mag_ang_R_X('AB')
+        self.combine_calculate_mag_ang_R_X('BC')
+        self.combine_calculate_mag_ang_R_X('CA')
 
-    def calculate_IL_real_imag(self, IL1, Ang_I1, IL2, Ang_I2, IL3, Ang_I3):
-        # Calculate real and imaginary parts of IL1, IL2, and IL3
-        IL1_Real = IL1 * math.cos(math.radians(Ang_I1))
-        IL1_Imag = IL1 * math.sin(math.radians(Ang_I1))
-        IL2_Real = IL2 * math.cos(math.radians(Ang_I2))
-        IL2_Imag = IL2 * math.sin(math.radians(Ang_I2))
-        IL3_Real = IL3 * math.cos(math.radians(Ang_I3))
-        IL3_Imag = IL3 * math.sin(math.radians(Ang_I3))
 
-        self.real_data.extend([IL1_Real, IL2_Real, IL3_Real])
-        self.imag_data.extend([IL1_Imag, IL2_Imag, IL3_Imag])
+    def _calculate_IL_real_imag(self, IL1, Ang_I1, IL2, Ang_I2, IL3, Ang_I3):
+        for IL, Ang_I in zip([IL1, IL2, IL3], [Ang_I1, Ang_I2, Ang_I3]):
+            IL_Real = IL * math.cos(math.radians(Ang_I))
+            IL_Imag = IL * math.sin(math.radians(Ang_I))
+            self.real_data.append(IL_Real)
+            self.imag_data.append(IL_Imag)
 
-    def calculate_V_real_imag(self, U1, Ang_U1, U2, Ang_U2, U3, Ang_U3):
-        # Calculate real and imaginary parts of V1, V2, and V3
-        V1_Real = U1 * math.cos(math.radians(Ang_U1))
-        V1_Imag = U1 * math.sin(math.radians(Ang_U1))
-        V2_Real = U2 * math.cos(math.radians(Ang_U2))
-        V2_Imag = U2 * math.sin(math.radians(Ang_U2))
-        V3_Real = U3 * math.cos(math.radians(Ang_U3))
-        V3_Imag = U3 * math.sin(math.radians(Ang_U3))
+    def _calculate_V_real_imag(self, U1, Ang_U1, U2, Ang_U2, U3, Ang_U3):
+        for U, Ang_U in zip([U1, U2, U3], [Ang_U1, Ang_U2, Ang_U3]):
+            V_Real = U * math.cos(math.radians(Ang_U))
+            V_Imag = U * math.sin(math.radians(Ang_U))
+            self.real_data.append(V_Real)
+            self.imag_data.append(V_Imag)
 
-        self.real_data.extend([V1_Real, V2_Real, V3_Real])
-        self.imag_data.extend([V1_Imag, V2_Imag, V3_Imag])
+    def _create_complex_data(self):
+        for i in range(6):
+            self.complex_data.append(complex(self.real_data[i], self.imag_data[i]))
 
-    def create_complex_data(self):
-        # Create complex numbers for IL1, IL2, IL3, V1, V2, and V3
-        IL1_complex = complex(self.real_data[0], self.imag_data[0])
-        IL2_complex = complex(self.real_data[1], self.imag_data[1])
-        IL3_complex = complex(self.real_data[2], self.imag_data[2])
 
-        V1_complex = complex(self.real_data[3], self.imag_data[3])
-        V2_complex = complex(self.real_data[4], self.imag_data[4])
-        V3_complex = complex(self.real_data[5], self.imag_data[5])
+    def calculate_LINE1_IN(self):
+        # Calculate LINE1_IN_Complex as the sum of IL1_complex, IL2_complex, and IL3_complex
+        # LINE1_IN_Complex = self.complex_data[0] + self.complex_data[1] + self.complex_data[2]
+        LINE1_IN_Complex = -24.3725335851725-2.63909674692661j
 
-        self.complex_data.extend([IL1_complex, IL2_complex, IL3_complex,
-                                  V1_complex, V2_complex, V3_complex])
+        # Calculate LINE1_IN_Imag and LINE1_IN_Real
+        LINE1_IN_Imag = LINE1_IN_Complex.imag
+        LINE1_IN_Real = LINE1_IN_Complex.real
+        LINE1_IN_Mag = math.sqrt(LINE1_IN_Real**2 + LINE1_IN_Imag**2)
+        LINE1_IN_Ang = math.degrees(math.atan2(LINE1_IN_Imag, LINE1_IN_Real))
 
-        LINE1_IN_Complex = IL1_complex + IL2_complex + IL3_complex
-        self.complex_data.append(LINE1_IN_Complex)
+
+        self.line1_IN_data.extend([LINE1_IN_Real, LINE1_IN_Imag, LINE1_IN_Complex, LINE1_IN_Mag, LINE1_IN_Ang])
+        
+    def calculate_k0_n0(self, LINE1_z0z1_mag, LINE1_z0z1_ang):
+        # Calculate k0 and n0 coefficients
+        LINE1_z0z1_ang_rad = math.radians(LINE1_z0z1_ang)
+        LINE1_k0 = cmath.rect(LINE1_z0z1_mag, LINE1_z0z1_ang_rad) - 1
+        LINE1_complex_number = complex(3, 0)
+        LINE1_n0 = LINE1_k0 / LINE1_complex_number
+        product_result = self.line1_IN_data[2] * LINE1_n0
+
+        self.line1_k0 = LINE1_k0
+        self.line1_n0 = LINE1_n0
+        self.product_result = product_result
+
+    def calculate_mag_ang_R_X(self, Z_type):
+        # Calculate sum_result
+        
+        
+        # Calculate LINE1_ZA or LINE1_ZB based on Z_type
+        if Z_type == 'A':
+            V_index = 3
+            IL_index = 0
+            data_list = self.data_ZA
+        elif Z_type == 'B':
+            V_index = 4
+            IL_index = 1
+            data_list = self.data_ZB
+        elif Z_type == 'C':
+            V_index = 5
+            IL_index = 2
+            data_list = self.data_ZC
+        else:
+            raise ValueError("Invalid Z_type. Must be 'A' or 'B' or 'C'.")
+        
+
+        sum_result = self.complex_data[IL_index] + self.product_result
+
+        Z_line = self.complex_data[V_index] / sum_result
+        
+        # Extract real and imaginary parts of LINE1_ZA or LINE1_ZB
+        Z_Real = Z_line.real
+        Z_Imag = Z_line.imag
+        data_list.extend([Z_Real, Z_Imag])
+        
+        # Calculate magnitude and angle of LINE1_ZA or LINE1_ZB
+        Z_Mag = abs(self.complex_data[V_index] / self.complex_data[IL_index])
+        Z_Ang = math.degrees(cmath.phase(self.complex_data[V_index] / self.complex_data[IL_index]))
+        
+        # Convert angle to radians
+        Z_Ang_radians = math.radians(Z_Ang)
+        
+        # Calculate R and X components of LINE1_ZA or LINE1_ZB
+        Z_R = Z_Mag * math.cos(Z_Ang_radians)
+        Z_X = Z_Mag * math.sin(Z_Ang_radians)
+        data_list.extend([Z_Mag, Z_Ang, Z_R, Z_X])
+
+    def combine_calculate_mag_ang_R_X(self, Z_type):
+        if Z_type == "AB":
+            V_topIndex = 4
+            V_bottomIndex = 3
+            IL_topIndex = 1
+            IL_bottomIndex = 0
+            data_list = self.data_ZAB
+        elif Z_type == "BC":
+            V_topIndex = 5
+            V_bottomIndex = 4
+            IL_topIndex = 2
+            IL_bottomIndex = 1
+            data_list = self.data_ZBC
+        elif Z_type == "CA":             
+            V_topIndex = 3
+            V_bottomIndex = 5
+            IL_topIndex = 0
+            IL_bottomIndex = 2
+            data_list = self.data_ZCA
+        else:
+            raise ValueError("Invalid Z_type. Must be 'AB' or 'BC' or 'CA'.")
+        
+        LINE1_Z_Mag = abs((self.complex_data[V_bottomIndex] - self.complex_data[V_topIndex]) / (self.complex_data[IL_bottomIndex] -self.complex_data[IL_topIndex]))
+        LINE1_diff_Vbot_Vtop = self.complex_data[V_bottomIndex] - self.complex_data[V_topIndex]
+        LINE1_diff_Ibot_Itop = self.complex_data[IL_bottomIndex] - self.complex_data[IL_topIndex]
+        LINE1_Z_Ang = math.degrees(math.atan2(LINE1_diff_Vbot_Vtop.imag, LINE1_diff_Vbot_Vtop.real) - math.atan2(LINE1_diff_Ibot_Itop.imag, LINE1_diff_Ibot_Itop.real))
+        LINE1_Z_Ang_radians = math.radians(LINE1_Z_Ang)
+        LINE1_Z_R = LINE1_Z_Mag * math.cos(LINE1_Z_Ang_radians)
+        LINE1_Z_X = LINE1_Z_Mag * math.sin(LINE1_Z_Ang_radians)
+        data_list.extend([LINE1_Z_Mag, LINE1_Z_Ang, LINE1_Z_R, LINE1_Z_X])
+
     def get_real_data(self):
         return self.real_data
 
@@ -60,3 +157,33 @@ class LineCalculation:
 
     def get_complex_data(self):
         return self.complex_data
+
+    def get_line1_IN_data(self):
+        return self.line1_IN_data
+    
+    def get_LINE1_k0(self):
+        return self.line1_k0
+
+    def get_LINE1_n0(self):
+        return self.line1_n0
+
+    def get_LINE1_PR(self):
+        return self.product_result
+    
+    def get_ZA_data(self):
+        return self.data_ZA
+
+    def get_ZB_data(self):
+        return self.data_ZB
+    
+    def get_ZC_data(self):
+        return self.data_ZC
+    
+    def get_ZAB_data(self):
+        return self.data_ZAB
+
+    def get_ZBC_data(self):
+        return self.data_ZBC
+
+    def get_ZCA_data(self):
+        return self.data_ZCA
