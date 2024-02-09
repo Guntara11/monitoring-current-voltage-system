5d77279aad88c608a7f210d81c262f6cfb412d50
ega guntara <ega.guntara394@gmail.com>
2024-02-09 18:22:11 +0700

    added select config

--- coba_plotly.py
+++ coba_plotly.py
@@ -1,3 +1,4 @@
+import os
 import json
 import dash
 from dash import dcc, html
@@ -12,15 +13,18 @@ mqtt_topic = "topic/sensor_data"
 
 
 
-# Read configuration data from config.json
-with open('config.json') as config_file:
-    config_data = json.load(config_file)
+# # Read configuration data from config.json
+# with open('config.json') as config_file:
+#     config_data = json.load(config_file)
 
 
 # Extract x and y coordinates and keys from config data
-config_keys = list(config_data.keys())
-config_x = [config_data[key]['x'] for key in config_data]
-config_y = [config_data[key]['y'] for key in config_data]
+# config_keys = list(config_data.keys())
+# config_x = [config_data[key]['x'] for key in config_data]
+# config_y = [config_data[key]['y'] for key in config_data]
+
+config_folder = 'config_imp'
+config_files = [os.path.join(config_folder, f) for f in os.listdir(config_folder) if f.endswith('.json')]
 
 lines_kanan = ["kanan_bawah_z1","kanan_samping_z1", "kanan_samping_z2", "kanan_samping_z3"]
 lines_kiri = ["kiri_atas_z3","kiri_samping_z1", "kiri_samping_z2", "kiri_samping_z3"]
@@ -34,26 +38,46 @@ lines_tengah2=["kiri_atas_z2", "reach_z2", "kanan_samping_z2"]
 app = dash.Dash(__name__)
 
 # Layout of the dashboard
+# Layout of the dashboard
 app.layout = html.Div(
     [
+        html.Label('Select Config File:'),
+        dcc.Dropdown(
+            id='config-dropdown',
+            options=[{'label': f, 'value': f} for f in config_files],
+            value=config_files[0] if config_files else None
+        ),
         dcc.Graph(id='live-update-graph'),
         dcc.Interval(
             id='interval-component',
-            interval=1*200,  # in milliseconds
+            interval=1*1000,  # in milliseconds
             n_intervals=0
         ),
     ]
 )
-
 # Initialize data storage
 mqtt_data = {'x': deque(maxlen=50), 'y': deque(maxlen=50)}
 
 # Callback to update the graph
 @app.callback(Output('live-update-graph', 'figure'),
-              [Input('interval-component', 'n_intervals')])
+              [Input('interval-component', 'n_intervals'),
+               Input('config-dropdown', 'value')])
 
 
-def update_graph(n):
+
+
+def update_graph(n, selected_config):
+    if not selected_config:
+        return {}
+
+    # Read configuration data from the selected config file
+    with open(selected_config) as config_file:
+        config_data = json.load(config_file)
+
+    config_keys = list(config_data.keys())
+    config_x = [config_data[key]['x'] for key in config_data]
+    config_y = [config_data[key]['y'] for key in config_data]
+
     # Scatter plot for MQTT data
     mqtt_trace = go.Scatter(x=list(mqtt_data['x']),
                             y=list(mqtt_data['y']),
@@ -117,6 +141,8 @@ def update_graph(n):
 
     return {'data': [mqtt_trace, config_trace, lines_kanan_trace, lines_kiri_trace, lines_atas_trace, lines_bawah_trace,
                      lines_tengah0_trace, lines_tengah1_trace, lines_tengah2_trace], 'layout': layout}
+    # return {'data': [config_trace], 'layout': layout}
+
 
 # MQTT data reception
 def on_message(client, userdata, message):
