78d0a569f054bb7f37f40107b2d62287f9dbafcb
dedesopain23 <sopiand772@gmail.com>
2024-02-10 12:14:47 +0700

    update coba_plotly

--- coba_plotly.py
+++ coba_plotly.py
@@ -26,28 +26,48 @@ mqtt_topic = "topic/sensor_data"
 config_folder = 'config_imp'
 config_files = [os.path.join(config_folder, f) for f in os.listdir(config_folder) if f.endswith('.json')]
 
-lines_kanan = ["kanan_bawah_z1","kanan_samping_z1", "kanan_samping_z2", "kanan_samping_z3"]
-lines_kiri = ["kiri_atas_z3","kiri_samping_z1", "kiri_samping_z2", "kiri_samping_z3"]
-lines_atas = ["kiri_atas_z3", "reach_z3", "kanan_samping_z3"]
-lines_bawah = ["kiri_bawah_z1", "point", "kanan_bawah_z1"]
-lines_tengah0 = ["point", "reach_z1", "reach_z2", "reach_z3"]
-lines_tengah1=["kiri_atas_z1", "reach_z1", "kanan_atas_z1"]
-lines_tengah2=["kiri_atas_z2", "reach_z2", "kanan_samping_z2"]
+# lines_kanan = ["kanan_bawah_z1","kanan_samping_z1", "kanan_samping_z2", "kanan_samping_z3"]
+# lines_kiri = ["kiri_atas_z3","kiri_samping_z1", "kiri_samping_z2", "kiri_samping_z3"]
+# lines_atas = ["kiri_atas_z3", "reach_z3", "kanan_samping_z3"]
+# lines_bawah = ["kiri_bawah_z1", "point", "kanan_bawah_z1"]
+# lines_tengah0 = ["point", "reach_z1", "reach_z2", "reach_z3"]
+# lines_tengah1=["kiri_atas_z1", "reach_z1", "kanan_atas_z1"]
+# lines_tengah2=["kiri_atas_z2", "reach_z2", "kanan_samping_z2"]
 
 # Initialize Dash app
 app = dash.Dash(__name__)
 
 # Layout of the dashboard
 # Layout of the dashboard
+# Layout of the dashboard
 app.layout = html.Div(
     [
-        html.Label('Select Config File:'),
-        dcc.Dropdown(
-            id='config-dropdown',
-            options=[{'label': f, 'value': f} for f in config_files],
-            value=config_files[0] if config_files else None
+        html.Div(
+            [
+                html.Label('Select Config File:'),
+                dcc.Dropdown(
+                    id='config-dropdown-phase-to-gnd',
+                    options=[{'label': f, 'value': f} for f in config_files],
+                    value=config_files[0] if config_files else None
+                ),
+                dcc.Graph(id='phase-to-gnd-graph')
+            ],
+            style={'width': '49%', 'display': 'inline-block'}
         ),
-        dcc.Graph(id='live-update-graph'),
+
+        html.Div(
+            [
+                html.Label('Select Config File:'),
+                dcc.Dropdown(
+                    id='config-dropdown-phase-to-phase',
+                    options=[{'label': f, 'value': f} for f in config_files],
+                    value=config_files[0] if config_files else None
+                ),
+                dcc.Graph(id='phase-to-phase-graph')
+            ],
+            style={'width': '49%', 'display': 'inline-block'}
+        ),
+
         dcc.Interval(
             id='interval-component',
             interval=1*1000,  # in milliseconds
@@ -55,92 +75,130 @@ app.layout = html.Div(
         ),
     ]
 )
+
 # Initialize data storage
 mqtt_data = {'x': deque(maxlen=50), 'y': deque(maxlen=50)}
 
-# Callback to update the graph
-@app.callback(Output('live-update-graph', 'figure'),
-              [Input('interval-component', 'n_intervals'),
-               Input('config-dropdown', 'value')])
+@app.callback(
+    [Output('phase-to-gnd-graph', 'figure'),
+     Output('phase-to-phase-graph', 'figure')],
+    [Input('interval-component', 'n_intervals'),
+     Input('config-dropdown-phase-to-gnd', 'value'),
+     Input('config-dropdown-phase-to-phase', 'value')])
 
 
 
+def update_graph(n, selected_config_phase_to_gnd, selected_config_phase_to_phase):
+    if not selected_config_phase_to_gnd or not selected_config_phase_to_phase:
+        return {}, {}
 
-def update_graph(n, selected_config):
-    if not selected_config:
-        return {}
+    # Read configuration data from the selected config file for phase-to-gnd
+    with open(selected_config_phase_to_gnd) as config_file_phase_to_gnd:
+        config_data_phase_to_gnd = json.load(config_file_phase_to_gnd)
 
-    # Read configuration data from the selected config file
-    with open(selected_config) as config_file:
-        config_data = json.load(config_file)
+    config_keys_phase_to_gnd = list(config_data_phase_to_gnd.keys())
+    config_x_phase_to_gnd = [config_data_phase_to_gnd[key]['x'] for key in config_data_phase_to_gnd]
+    config_y_phase_to_gnd = [config_data_phase_to_gnd[key]['y'] for key in config_data_phase_to_gnd]
 
-    config_keys = list(config_data.keys())
-    config_x = [config_data[key]['x'] for key in config_data]
-    config_y = [config_data[key]['y'] for key in config_data]
+    # Read configuration data from the selected config file for phase-to-phase
+    with open(selected_config_phase_to_phase) as config_file_phase_to_phase:
+        config_data_phase_to_phase = json.load(config_file_phase_to_phase)
 
+    config_keys_phase_to_phase = list(config_data_phase_to_phase.keys())
+    config_x_phase_to_phase = [config_data_phase_to_phase[key]['x'] for key in config_data_phase_to_phase]
+    config_y_phase_to_phase = [config_data_phase_to_phase[key]['y'] for key in config_data_phase_to_phase]
+    
     # Scatter plot for MQTT data
     mqtt_trace = go.Scatter(x=list(mqtt_data['x']),
                             y=list(mqtt_data['y']),
                             mode='lines+markers',
                             name='Voltage vs Current')
 
-    # Scatter plot for config data
-    config_trace = go.Scatter(x=config_x, y=config_y,
-                              mode='markers',
-                              name='Config Points',
-                              marker=dict(color='red', size=10))
+    # Scatter plot for config data for phase-to-gnd
+    config_trace_phase_to_gnd = go.Scatter(x=config_x_phase_to_gnd, y=config_y_phase_to_gnd,
+                                            mode='markers',
+                                            name='Config Points (Phase-to-Gnd)',
+                                            marker=dict(color='red', size=10))
+
+    # Scatter plot for config data for phase-to-phase
+    config_trace_phase_to_phase = go.Scatter(x=config_x_phase_to_phase, y=config_y_phase_to_phase,
+                                              mode='markers',
+                                              name='Config Points (Phase-to-Phase)',
+                                              marker=dict(color='blue', size=10))
+#  # Add text annotations for config points
+#     annotations = [dict(x=x, y=y, text=key, showarrow=True, arrowhead=2, arrowcolor='black', arrowsize=1)
+#                    for key, x, y in zip(config_keys, config_x, config_y)]
     
- # Add text annotations for config points
-    annotations = [dict(x=x, y=y, text=key, showarrow=True, arrowhead=2, arrowcolor='black', arrowsize=1)
-                   for key, x, y in zip(config_keys, config_x, config_y)]
+    # # Lines to connect specific points
+    # lines_kanan_trace = go.Scatter(x=[config_data[key]['x'] for key in lines_kanan],
+    #                         y=[config_data[key]['y'] for key in lines_kanan],
+    #                         mode='lines',
+    #                         name='Lines',
+    #                         line=dict(color='blue', width=2))
+    # lines_kiri_trace = go.Scatter(x=[config_data[key]['x'] for key in lines_kiri],
+    #                         y=[config_data[key]['y'] for key in lines_kiri],
+    #                         mode='lines',
+    #                         name='Lines',
+    #                         line=dict(color='blue', width=2))
+    # lines_atas_trace = go.Scatter(x=[config_data[key]['x'] for key in lines_atas],
+    #                         y=[config_data[key]['y'] for key in lines_atas],
+    #                         mode='lines',
+    #                         name='Lines',
+    #                         line=dict(color='blue', width=2))
+    # lines_bawah_trace = go.Scatter(x=[config_data[key]['x'] for key in lines_bawah],
+    #                         y=[config_data[key]['y'] for key in lines_bawah],
+    #                         mode='lines',
+    #                         name='Lines',
+    #                         line=dict(color='blue', width=2))
+    # lines_tengah0_trace = go.Scatter(x=[config_data[key]['x'] for key in lines_tengah0],
+    #                         y=[config_data[key]['y'] for key in lines_tengah0],
+    #                         mode='lines',
+    #                         name='Lines',
+    #                         line=dict(color='blue', width=2)) 
+    # lines_tengah1_trace = go.Scatter(x=[config_data[key]['x'] for key in lines_tengah1],
+    #                         y=[config_data[key]['y'] for key in lines_tengah1],
+    #                         mode='lines',
+    #                         name='Lines',
+    #                         line=dict(color='blue', width=2))
+
+    # lines_tengah2_trace = go.Scatter(x=[config_data[key]['x'] for key in lines_tengah2],
+    #                         y=[config_data[key]['y'] for key in lines_tengah2],
+    #                         mode='lines',
+    #                         name='Lines',
+    #                         line=dict(color='blue', width=2))
+
+
     
-    # Lines to connect specific points
-    lines_kanan_trace = go.Scatter(x=[config_data[key]['x'] for key in lines_kanan],
-                            y=[config_data[key]['y'] for key in lines_kanan],
-                            mode='lines',
-                            name='Lines',
-                            line=dict(color='blue', width=2))
-    lines_kiri_trace = go.Scatter(x=[config_data[key]['x'] for key in lines_kiri],
-                            y=[config_data[key]['y'] for key in lines_kiri],
-                            mode='lines',
-                            name='Lines',
-                            line=dict(color='blue', width=2))
-    lines_atas_trace = go.Scatter(x=[config_data[key]['x'] for key in lines_atas],
-                            y=[config_data[key]['y'] for key in lines_atas],
-                            mode='lines',
-                            name='Lines',
-                            line=dict(color='blue', width=2))
-    lines_bawah_trace = go.Scatter(x=[config_data[key]['x'] for key in lines_bawah],
-                            y=[config_data[key]['y'] for key in lines_bawah],
-                            mode='lines',
-                            name='Lines',
-                            line=dict(color='blue', width=2))
-    lines_tengah0_trace = go.Scatter(x=[config_data[key]['x'] for key in lines_tengah0],
-                            y=[config_data[key]['y'] for key in lines_tengah0],
-                            mode='lines',
-                            name='Lines',
-                            line=dict(color='blue', width=2)) 
-    lines_tengah1_trace = go.Scatter(x=[config_data[key]['x'] for key in lines_tengah1],
-                            y=[config_data[key]['y'] for key in lines_tengah1],
-                            mode='lines',
-                            name='Lines',
-                            line=dict(color='blue', width=2))
 
-    lines_tengah2_trace = go.Scatter(x=[config_data[key]['x'] for key in lines_tengah2],
-                            y=[config_data[key]['y'] for key in lines_tengah2],
-                            mode='lines',
-                            name='Lines',
-                            line=dict(color='blue', width=2))
+    # Lines to connect specific points for phase-to-gnd
+    lines_trace_phase_to_gnd = go.Scatter(x=config_x_phase_to_gnd, y=config_y_phase_to_gnd,
+                                          mode='lines',
+                                          name='Lines (Phase-to-Gnd)',
+                                          line=dict(color='green', width=2))
 
-    layout = go.Layout(title='Live Dashboard - Voltage vs Current with Config Points',
-                       xaxis=dict(title='Voltage'),
-                       yaxis=dict(title='Current'),
-                       showlegend=True,
-                        width=1400,
-                       height=800)
+    # Lines to connect specific points for phase-to-phase
+    lines_trace_phase_to_phase = go.Scatter(x=config_x_phase_to_phase, y=config_y_phase_to_phase,
+                                             mode='lines',
+                                             name='Lines (Phase-to-Phase)',
+                                             line=dict(color='orange', width=2))
 
-    return {'data': [mqtt_trace, config_trace, lines_kanan_trace, lines_kiri_trace, lines_atas_trace, lines_bawah_trace,
-                     lines_tengah0_trace, lines_tengah1_trace, lines_tengah2_trace], 'layout': layout}
+    layout_phase_to_gnd = go.Layout(title='Live Dashboard - Voltage vs Current with Config Points (Phase-to-Gnd)',
+                                    xaxis=dict(title='Voltage'),
+                                    yaxis=dict(title='Current'),
+                                    showlegend=True,
+                                    width=1250,
+                                    height=850)
+
+    layout_phase_to_phase = go.Layout(title='Live Dashboard - Voltage vs Current with Config Points (Phase-to-Phase)',
+                                      xaxis=dict(title='Voltage'),
+                                      yaxis=dict(title='Current'),
+                                      showlegend=True,
+                                      width=1250,
+                                      height=850)
+
+    return {'data': [mqtt_trace, config_trace_phase_to_gnd, lines_trace_phase_to_gnd], 'layout': layout_phase_to_gnd}, \
+           {'data': [mqtt_trace, config_trace_phase_to_phase, lines_trace_phase_to_phase], 'layout': layout_phase_to_phase}
+
     # return {'data': [config_trace], 'layout': layout}
 
 
--- phase.py
+++ phase.py
@@ -1,86 +1,19 @@
-# import json
-# import dash
-# from dash import dcc, html
-# from dash.dependencies import Input, Output
-# import plotly.graph_objs as go
-
-# # Read configuration data from config.json
-# with open('phase_to_phase.json') as config_file:
-#     config_data = json.load(config_file)
-
-# # Extract x and y coordinates and keys from config data
-# config_keys = list(config_data.keys())
-# config_x = [config_data[key]['x'] for key in config_data]
-# config_y = [config_data[key]['y'] for key in config_data]
-
-# # Initialize Dash app
-# app = dash.Dash(__name__)
-
-# # Layout of the dashboard
-# app.layout = html.Div(
-#     [
-#         dcc.Graph(id='live-update-graph'),
-#         dcc.Interval(
-#             id='interval-component',
-#             interval=1 * 200,  # in milliseconds
-#             n_intervals=0
-#         ),
-#     ]
-# )
-
-# # Callback to update the graph
-# @app.callback(Output('live-update-graph', 'figure'),
-#               [Input('interval-component', 'n_intervals')])
-# def update_graph(n):
-#     # Scatter plot for config data
-#     config_trace = go.Scatter(x=config_x, y=config_y,
-#                               mode='markers',
-#                               name='Config Points',
-#                               marker=dict(color='red', size=10))
-
-#     # Lines to connect specific points
-#     lines_traces = []
-#     for key, value in config_data.items():
-#         if key.startswith("lines_"):
-#             lines_trace = go.Scatter(x=[config_data[key]['x'], config_data[key[:-3]]['x']],
-#                                      y=[config_data[key]['y'], config_data[key[:-3]]['y']],
-#                                      mode='lines',
-#                                      name='Lines',
-#                                      line=dict(color='blue', width=2))
-#             lines_traces.append(lines_trace)
-
-#     # Connect each point with a line
-#     connected_points = []
-#     for i in range(len(config_x) - 1):
-#         connected_points.append(go.Scatter(x=[config_x[i], config_x[i + 1]],
-#                                            y=[config_y[i], config_y[i + 1]],
-#                                            mode='lines',
-#                                            line=dict(color='green', width=2)))
-
-#     layout = go.Layout(title='Live Dashboard - Voltage vs Current with Config Points',
-#                        xaxis=dict(title='Voltage'),
-#                        yaxis=dict(title='Current'),
-#                        showlegend=True,
-#                        width=1400,
-#                        height=800)
-
-#     return {'data': [config_trace] + lines_traces + connected_points, 'layout': layout}
-
-# # Run the app
-# if __name__ == '__main__':
-#     app.run_server(debug=True)
-
+import os
 import json
 import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output
+import paho.mqtt.client as mqtt
 import plotly.graph_objs as go
 from collections import deque
-import paho.mqtt.client as mqtt
 
 # MQTT Broker information
 mqtt_broker = "broker.emqx.io"
 mqtt_topic = "topic/sensor_data"
+
+# Folder containing JSON configuration files
+config_folder = 'config_imp'
+config_files = [os.path.join(config_folder, f) for f in os.listdir(config_folder) if f.endswith('.json')]
 
 # Initialize Dash app
 app = dash.Dash(__name__)
@@ -88,26 +21,69 @@ app = dash.Dash(__name__)
 # Layout of the dashboard
 app.layout = html.Div(
     [
-        dcc.Graph(id='phase-to-phase-graph'),
-        dcc.Graph(id='phase-to-gnd-graph'),
+        html.Div(
+            [
+                html.Label('Select Config File:'),
+                dcc.Dropdown(
+                    id='config-dropdown-phase-to-gnd',
+                    options=[{'label': f, 'value': f} for f in config_files],
+                    value=config_files[0] if config_files else None
+                ),
+                dcc.Graph(id='phase-to-gnd-graph')
+            ],
+            style={'width': '49%', 'display': 'inline-block'}
+        ),
+
+        html.Div(
+            [
+                html.Label('Select Config File:'),
+                dcc.Dropdown(
+                    id='config-dropdown-phase-to-phase',
+                    options=[{'label': f, 'value': f} for f in config_files],
+                    value=config_files[0] if config_files else None
+                ),
+                dcc.Graph(id='phase-to-phase-graph')
+            ],
+            style={'width': '49%', 'display': 'inline-block'}
+        ),
+
         dcc.Interval(
             id='interval-component',
-            interval=1 * 200,  # in milliseconds
+            interval=1*1000,  # in milliseconds
             n_intervals=0
         ),
     ]
 )
 
-# Initialize data storage
+# Initialize data storage for MQTT data
 mqtt_data = {'x': deque(maxlen=50), 'y': deque(maxlen=50)}
 
-# Callback to update the graph for phase_to_phase
-@app.callback(Output('phase-to-phase-graph', 'figure'),
-              [Input('interval-component', 'n_intervals')])
-def update_phase_to_phase_graph(n):
-    # Read configuration data from phase_to_phase.json
-    with open('phase_to_phase.json') as config_file:
-        config_data_phase_to_phase = json.load(config_file)
+@app.callback(
+    [Output('phase-to-gnd-graph', 'figure'),
+     Output('phase-to-phase-graph', 'figure')],
+    [Input('interval-component', 'n_intervals'),
+     Input('config-dropdown-phase-to-gnd', 'value'),
+     Input('config-dropdown-phase-to-phase', 'value')])
+
+def update_graph(n, selected_config_phase_to_gnd, selected_config_phase_to_phase):
+    if not selected_config_phase_to_gnd or not selected_config_phase_to_phase:
+        return {}, {}
+
+    # Read configuration data from the selected config file for phase-to-gnd
+    with open(selected_config_phase_to_gnd) as config_file_phase_to_gnd:
+        config_data_phase_to_gnd = json.load(config_file_phase_to_gnd)
+
+    config_keys_phase_to_gnd = list(config_data_phase_to_gnd.keys())
+    config_x_phase_to_gnd = [config_data_phase_to_gnd[key]['x'] for key in config_data_phase_to_gnd]
+    config_y_phase_to_gnd = [config_data_phase_to_gnd[key]['y'] for key in config_data_phase_to_gnd]
+
+    # Read configuration data from the selected config file for phase-to-phase
+    with open(selected_config_phase_to_phase) as config_file_phase_to_phase:
+        config_data_phase_to_phase = json.load(config_file_phase_to_phase)
+
+    config_keys_phase_to_phase = list(config_data_phase_to_phase.keys())
+    config_x_phase_to_phase = [config_data_phase_to_phase[key]['x'] for key in config_data_phase_to_phase]
+    config_y_phase_to_phase = [config_data_phase_to_phase[key]['y'] for key in config_data_phase_to_phase]
 
     # Scatter plot for MQTT data
     mqtt_trace = go.Scatter(x=list(mqtt_data['x']),
@@ -115,65 +91,47 @@ def update_phase_to_phase_graph(n):
                             mode='lines+markers',
                             name='Voltage vs Current')
 
-    # Scatter plot for config data from phase_to_phase.json
-    config_trace_phase_to_phase = go.Scatter(x=[config_data_phase_to_phase[key]['x'] for key in config_data_phase_to_phase],
-                                              y=[config_data_phase_to_phase[key]['y'] for key in config_data_phase_to_phase],
+    # Scatter plot for config data for phase-to-gnd
+    config_trace_phase_to_gnd = go.Scatter(x=config_x_phase_to_gnd, y=config_y_phase_to_gnd,
+                                            mode='markers',
+                                            name='Config Points (Phase-to-Gnd)',
+                                            marker=dict(color='red', size=10))
+
+    # Scatter plot for config data for phase-to-phase
+    config_trace_phase_to_phase = go.Scatter(x=config_x_phase_to_phase, y=config_y_phase_to_phase,
                                               mode='markers',
-                                              name='Config Points - Phase to Phase',
-                                              marker=dict(color='red', size=10))
+                                              name='Config Points (Phase-to-Phase)',
+                                              marker=dict(color='blue', size=10))
 
-    # Lines to connect specific points
-    lines_trace_phase_to_phase = go.Scatter(x=[config_data_phase_to_phase[key]['x'] for key in config_data_phase_to_phase],
-                                            y=[config_data_phase_to_phase[key]['y'] for key in config_data_phase_to_phase],
-                                            mode='lines',
-                                            name='Lines - Phase to Phase',
-                                            line=dict(color='blue', width=2))
-
-    layout = go.Layout(title='Phase to Phase - Voltage vs Current with Config Points',
-                       xaxis=dict(title='Voltage'),
-                       yaxis=dict(title='Current'),
-                       showlegend=True,
-                       width=1400,
-                       height=800)
-
-    return {'data': [mqtt_trace, config_trace_phase_to_phase, lines_trace_phase_to_phase], 'layout': layout}
-
-# Callback to update the graph for phase_to_gnd
-@app.callback(Output('phase-to-gnd-graph', 'figure'),
-              [Input('interval-component', 'n_intervals')])
-def update_phase_to_gnd_graph(n):
-    # Read configuration data from phase_to_gnd.json
-    with open('phase_to_gnd.json') as config_file:
-        config_data_phase_to_gnd = json.load(config_file)
-
-    # Scatter plot for MQTT data
-    mqtt_trace = go.Scatter(x=list(mqtt_data['x']),
-                            y=list(mqtt_data['y']),
-                            mode='lines+markers',
-                            name='Voltage vs Current')
-
-    # Scatter plot for config data from phase_to_gnd.json
-    config_trace_phase_to_gnd = go.Scatter(x=[config_data_phase_to_gnd[key]['x'] for key in config_data_phase_to_gnd],
-                                           y=[config_data_phase_to_gnd[key]['y'] for key in config_data_phase_to_gnd],
-                                           mode='markers',
-                                           name='Config Points - Phase to Ground',
-                                           marker=dict(color='blue', size=10))
-
-    # Lines to connect specific points
-    lines_trace_phase_to_gnd = go.Scatter(x=[config_data_phase_to_gnd[key]['x'] for key in config_data_phase_to_gnd],
-                                          y=[config_data_phase_to_gnd[key]['y'] for key in config_data_phase_to_gnd],
+    # Lines to connect specific points for phase-to-gnd
+    lines_trace_phase_to_gnd = go.Scatter(x=config_x_phase_to_gnd, y=config_y_phase_to_gnd,
                                           mode='lines',
-                                          name='Lines - Phase to Ground',
+                                          name='Lines (Phase-to-Gnd)',
                                           line=dict(color='green', width=2))
 
-    layout = go.Layout(title='Phase to Ground - Voltage vs Current with Config Points',
-                       xaxis=dict(title='Voltage'),
-                       yaxis=dict(title='Current'),
-                       showlegend=True,
-                       width=1400,
-                       height=800)
+    # Lines to connect specific points for phase-to-phase
+    lines_trace_phase_to_phase = go.Scatter(x=config_x_phase_to_phase, y=config_y_phase_to_phase,
+                                             mode='lines',
+                                             name='Lines (Phase-to-Phase)',
+                                             line=dict(color='orange', width=2))
 
-    return {'data': [mqtt_trace, config_trace_phase_to_gnd, lines_trace_phase_to_gnd], 'layout': layout}
+    layout_phase_to_gnd = go.Layout(title='Live Dashboard - Voltage vs Current with Config Points (Phase-to-Gnd)',
+                                    xaxis=dict(title='Voltage'),
+                                    yaxis=dict(title='Current'),
+                                    showlegend=True,
+                                    width=1250,
+                                    height=850)
+
+    layout_phase_to_phase = go.Layout(title='Live Dashboard - Voltage vs Current with Config Points (Phase-to-Phase)',
+                                      xaxis=dict(title='Voltage'),
+                                      yaxis=dict(title='Current'),
+                                      showlegend=True,
+                                      width=1250,
+                                      height=850)
+
+    return {'data': [mqtt_trace, config_trace_phase_to_gnd, lines_trace_phase_to_gnd], 'layout': layout_phase_to_gnd}, \
+           {'data': [mqtt_trace, config_trace_phase_to_phase, lines_trace_phase_to_phase], 'layout': layout_phase_to_phase}
+
 
 # MQTT data reception
 def on_message(client, userdata, message):
